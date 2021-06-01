8db9bb90545ebb12cdaca16004e3f7073a32263f
giodueck <giodueck@gmail.com>
2021-06-01 10:15:17 -0400

    Added proper sequence operations to entitygen

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Compatible con el diseño del Challenge 5
 
 # Funcionamiento
 persongen: generate crea una persona, leadergen y scoutgen generan filas a partir de una lista de person_ids. Para obtener un person/scout/leader_id valido se consulta e incrementa la secuencia asociada.
+entitygen: cada clase crea filas en la tabla de su nombre, division adicionalmente crea filas en division_category. Funciona de forma similar a persongen
 
 # Dependencies
 command: "pip install pkgname"
--- entitygen.py
+++ entitygen.py
@@ -2,35 +2,98 @@ import placenamegen as pngen
 import psycopg2
 
 class office:
-    def generate(leader_id: int, vice_leader_id: int, region: str = None) -> str:
+    def generate(cursor, c: int, leader_ids: list, vice_leader_ids: list, ids: list, region: str = None) -> str:
         '''Generate the SQL command to insert a new office.'''
 
-        retstr = "INSERT INTO office (leader_id, vice_leader_id, region) VALUES ("
+        retstr = "INSERT INTO office (office_id, leader_id, vice_leader_id, region) VALUES"
 
-        if region is None:
-            region = pngen.generate()
+        for i in range(c):
+            # formatting
+            if i > 0:
+                retstr += ", "
+
+            # office_id
+            cursor.execute("SELECT * FROM nextval('office_office_id_seq');")
+            seq = cursor.fetchone()
+            id = seq[0]
+            ids.append(id)
+
+            # region
+            if region is None:
+                region = pngen.generate()
+
+            # add to SQL string
+            retstr += "(" + str(id) + ',' + str(leader_ids[i]) + ',' + str(vice_leader_ids[i]) + ",'" + region + "')"
         
-        return retstr + str(leader_id) + ',' + str(vice_leader_id) + ",'" + region + "');"
+        retstr += ";"
+        return retstr
 
 class outpost:
-    def generate(office_id: int, leader_id: int, vice_leader_id: int, outpost_number: int) -> str:
+    def generate(cursor, c: int, office_id: int, leader_ids: list, vice_leader_ids: list, ids: list, first_outpost_number: int = None) -> str:
         '''Generate the SQL command to insert a new outpost.'''
 
-        retstr = "INSERT INTO outpost (office_id, leader_id, vice_leader_id, outpost_number) VALUES "
+        retstr = "INSERT INTO outpost (outpost_id, office_id, leader_id, vice_leader_id, outpost_number) VALUES"
 
-        return retstr + '(' + str(office_id) + ',' + str(leader_id) + ',' + str(vice_leader_id) + ',' + str(outpost_number) + ");"
+        for i in range(c):
+            # formatting
+            if i > 0:
+                retstr += ", "
+
+            # outpost_id and number
+            cursor.execute("SELECT * FROM nextval('outpost_outpost_id_seq');")
+            seq = cursor.fetchone()
+            id = seq[0]
+            ids.append(id)
+            if i == 0 and first_outpost_number is None:
+                first_outpost_number = id
+
+            # add to SQL string
+            retstr += "(" + str(id) + ',' + str(office_id) + ',' + str(leader_ids[i]) + ',' + str(vice_leader_ids[i]) + ',' + str(first_outpost_number + i) + ")"
+        
+        retstr += ";"
+        return retstr
 
 class division:
-    def categorygen(name: str) -> str:
+    def categorygen(cursor, c: int, names: list, ids: list) -> str:
         '''Generate the SQL command to insert a new division category.'''
         
-        retstr = "INSERT INTO division_category (name) VALUES "
+        retstr = "INSERT INTO division_category (division_category_id, name) VALUES "
 
-        return retstr + '(' + name + ");"
+        for i in range(c):
+            # formatting
+            if i > 0:
+                retstr += ", "
+
+            # division_category_id
+            cursor.execute("SELECT * FROM nextval('division_category_division_category_id_seq_1');")
+            seq = cursor.fetchone()
+            id = seq[0]
+            ids.append(id)
+
+            # add to SQL string
+            retstr += "(" + str(id) + ",'" + names[i] + "')"
+        
+        retstr += ";"
+        return retstr
     
-    def generate(division_category_id: int, outpost_id: int, leader_id: int, vice_leader_id: int) -> str:
+    def generate(cursor, c: int, division_category_id: int, outpost_ids: list, leader_ids: list, vice_leader_ids: list, ids: list) -> str:
         '''Generate the SQL command to insert a new division.'''
 
-        retstr = "INSERT INTO division (division_category_id, outpost_id, leader_id, vice_leader_id) VALUES "
+        retstr = "INSERT INTO division (division_id, division_category_id, outpost_id, leader_id, vice_leader_id) VALUES "
 
-        return retstr + '(' + str(division_category_id) + ',' + str(outpost_id) + ',' + str(leader_id) + ',' + str(vice_leader_id) + ");"
+        for i in range(c):
+            # formatting
+            if i > 0:
+                retstr += ", "
+
+            # division_id
+            cursor.execute("SELECT * FROM nextval('division_division_id_seq');")
+            seq = cursor.fetchone()
+            id = seq[0]
+            ids.append(id)
+
+            # add to SQL string
+            retstr += '(' + str(id) + ',' + str(division_category_id) + ',' + str(outpost_ids[i]) + ',' + str(leader_ids[i]) + ',' + str(vice_leader_ids[i]) + ")"
+        
+        retstr += ";"
+        return retstr
